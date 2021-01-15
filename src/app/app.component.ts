d690f527f74096bc120ba59bd4d8c712a355ad6c
AlexisNarvaez <alexisnarvaez97@hotmail.com>
2021-01-15 12:13:58 -0600

    Download langs into tinnymce folder langs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { AsyncSubject, Subject } from 'rxjs';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-
-  private editorSubject: Subject<any> = new AsyncSubject();
+  public init = {};
 
   title = 'dummie-project';
 
-  form: FormGroup;
+  // pt_PT
+  // fr_FR
+  // es_MX
+  // english default
 
-  public myForm = new FormGroup({
-    title: new FormControl('', Validators.required),
-    body: new FormControl('', Validators.required)
-  });
-
-  constructor(private fb: FormBuilder) {
-
-  }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.init = {
+      selector: 'textarea',
+      plugins: [
+        'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+        'searchreplace wordcount visualblocks visualchars code fullscreen',
+        'insertdatetime media nonbreaking save table directionality',
+        'emoticons template paste textpattern',
+      ],
+      menubar: true,
+      directionality : 'ltr', // English
+      base_url: '/tinymce',
+      suffix: '.min',
+      toolbar:
+        'undo redo | formatselect | ' +
+        ' bold italic | alignleft aligncenter ' +
+        ' alignright alignjustify | bullist numlist outdent indent |' +
+        ' removeformat',
+    };
   }
-
-  onSubmit(): void {
-    console.log(this.form);
-  }
-
 }
