fef012322ef39ff13628281fb326997477562555
AlexisNarvaez <alexisnarvaez97@hotmail.com>
2021-01-15 11:12:17 -0600

    Add module tinny-mce

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AsyncSubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  private editorSubject: Subject<any> = new AsyncSubject();
+
   title = 'dummie-project';
+
+  form: FormGroup;
+
+  public myForm = new FormGroup({
+    title: new FormControl('', Validators.required),
+    body: new FormControl('', Validators.required)
+  });
+
+  constructor(private fb: FormBuilder) {
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    console.log(this.form);
+  }
+
 }
